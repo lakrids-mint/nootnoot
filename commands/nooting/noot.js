5f87f9d856eaf6df1e91fe616e2420d29cc392a2
lakrids-mint <mettepernille@hotmail.com>
2021-03-31 18:54:31 +0200

    meh

--- commands/nooting/noot.js
+++ commands/nooting/noot.js
@@ -1,11 +1,8 @@
-const pinguGif = require('../../assets/nootnoot.gif');
-
 module.exports = {
     name: 'noot',
     description: 'Get a simple noot',
     execute(message, args) {
 
         message.channel.send('noot!');
-        message.channel.send(pinguGif);
     },
 };
