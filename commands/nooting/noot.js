89d291d5bec1a9d925bcf923697d8b8a47ade889
Magnus <magnus.tk@hotmail.com>
2021-04-02 21:53:20 +0200

    random strings and custom emotes

--- commands/nooting/noot.js
+++ commands/nooting/noot.js
@@ -1,8 +1,14 @@
-module.exports = {
+const utils = require('../../utils/utils');
+
+
+    
+    module.exports = {
     name: 'noot',
     description: 'Get a simple noot',
+    list_of_noots: ["NOOT!", "noot", "blah noot", "not a good day to noot"],
     execute(message, args) {
-
-        message.channel.send('noot!');
+            message.channel.send("<a:fistface:818638861646823494>")
+        //message.channel.send(this.list_of_noots[utils.getRandomNumber(this.list_of_noots.length)]);
     },
 };
+
